a985a80a7a5bad546653b76921eaabffd32d9b8d
Vhartzz <vhartzamorimg2@gmail.com>
2019-03-17 12:26:56 -0300

    correção

--- system_login_default.py
+++ system_login_default.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python
+#!/usr/bin/python
 """
     author : Vitor Amorim 'Vhartz' <vhartzamorimg2@gmail.com>
     version : 0.1
@@ -9,7 +9,6 @@ from time import strftime as time
 import os
 import subprocess
 
-# função de janela de seleção
 def selection_modules():
     select = Tk()
     select.title("Home")
@@ -17,25 +16,25 @@ def selection_modules():
     select.resizable(False,False)
     #select.configure(background='')
 
-    # função exit da tela de seleção
+    # exit
     def logout_exit():
         select.destroy()
 
     lbl_welcome =Label(select, text="Bem-Vindo Vitor Amorim",  font='Times 20 bold italic')
     lbl_welcome.pack(side=TOP)
 
-    #setando atualização do relogio
+    #set update time
     def tictac():
         new = time("%H:%M:%S")
         if rlg['text'] != new:
             rlg['text'] = new
         rlg.after(100,tictac)
 
-    #  label do relogio
+    #  label for time
     rlg = Label(select, text=time("%H:%M:%S"), font='Times 15 bold')
     rlg.pack(side=TOP)
 
-    # inicializção do pkg helper 
+    #  pkg helper
     def pkg_h():
         import pkg_helper
 
@@ -51,16 +50,16 @@ def selection_modules():
     #teste de internet
     def speed():
         os.system('speedtest')
-    
+
     speed_test = Button(select, text='Speed Test', font=('arial',9,'bold'), command = speed)
     speed_test.pack(side=TOP, padx=10, pady=10)
     #open studio code
     def code():
         os.system("/opt/visual-studio-code/code '%f'")
-    
+
     studio_code = Button(select, text='Studio Code', font=('arial',9,'bold'), command = code )
     studio_code.pack(side=TOP, padx=10, pady=10)
-    #gravador de iso incializavel com comando dd
+    # dd command
 
     def dd_comand():
         window_dd = Tk()
@@ -70,19 +69,19 @@ def selection_modules():
 
         lbl0 = Label(window_dd, text='Insira o Caminho da ISO :', font='Times 11 bold ' )
         lbl0.pack(side=TOP, padx=10, pady=5)
-        
+
         dd_insert = Entry(window_dd, width=40)
         dd_insert.pack(side=TOP, padx=10, pady=5)
-        
+
         lblex1 = Label(window_dd, text='ex : /local/da/imagem.iso')
         lblex1.pack(side=TOP, padx=5, pady=0)
 
         lbl1 = Label(window_dd, text='Insira a Unidade a ser Gravada :', font='Times 11 bold ' )
         lbl1.pack(side=TOP, padx=10, pady=5)
-        
+
         dd_insertt = Entry(window_dd, width=40)
         dd_insertt.pack(side=TOP, padx=10, pady=5)
-        
+
         lblex2 = Label(window_dd, text='ex : /dev/sdX')
         lblex2.pack(side=TOP, padx=5, pady=0)
 
@@ -96,36 +95,34 @@ def selection_modules():
             sub = subprocess.check_output('sudo dd if=%s of=%s bs=4M status=progress' %(first,second), shell = True)
 
             lb = Label(window_sub, text=sub)
-            lb.pack(side=TOP, padx=10, pady=10)   
+            lb.pack(side=TOP, padx=10, pady=10)
 
-            window_sub.mainloop()   
-                
+            window_sub.mainloop()
 
         lbl2 = Button(window_dd, text='Gravar!', command = gravar)
         lbl2.pack(side=TOP, padx=10, pady=5)
 
         window_dd.mainloop()
-                
+
     comand_dd = Button(select, text='DD', font=('arial',9,'bold'), command = dd_comand)
     comand_dd.pack(side=TOP, padx=10, pady=10)
-      
+
     logout = Button(select, text="Logout", font=('arial',9,'bold'), command = logout_exit)
     logout.pack(side=BOTTOM, padx=10, pady=10)
 
     tictac()
     select.mainloop()
-###################################------------------------########################################         
 
-# função de login
+# login
 def iniciar():
     login_base = Tk()
     login_base.title('Login Snake')
     login_base.geometry("200x180")
     login_base.resizable(False,False)
     #login_base.configure(background='#858585')
-    #login_base.iconbitmap(r':C:\Users\Vitor Amorim\Google Drive\Python\Snare\icon\icon_login_base.ico')
-   
-    # função exit do login
+    #login_base.iconbitmap(r':')
+
+    # exit
     def exit():
         login_base.destroy()
     #verification user and passwd
@@ -135,12 +132,15 @@ def iniciar():
         if usr == 'admin' and pswd == 'admin':
             print("Started Sucessfull")
             selection_modules()
-            login_base.destroy()
+            #login_base.destroy()
+        else:
+            print('User or passwd error')
+        exit()
 
     lb1 = Label(text='Login', font='Times 15 bold italic' )
     lb1.pack(side=TOP)
 
-    lb2 = Label(text='Usurario')
+    lb2 = Label(text='Usuario')
     lb2.pack(side=TOP)
 
     user = Entry()
@@ -154,11 +154,10 @@ def iniciar():
 
     enter = Button(text='Entrar', command = ent)
     enter.pack(side=LEFT, pady=10, padx=30)
-    
+
     exi = Button(text='Sair', command = exit)
     exi.pack(side=RIGHT, pady=10 ,padx=30)
 
     login_base.mainloop()
-########################################---------------------------------#####################################
 
 iniciar()
